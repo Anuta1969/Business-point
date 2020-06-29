f4b8eae04058c100cf910e601bf951374393e41b
Anna <example@@gmail.com>
2020-06-29 15:14:38 +0300

    third

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
   asNavFor: '.post__slider2',
  
 
-  // prevArrow: '<button type="button" class="slick-prev"><img src="icon/press.png"></button>',
+  prevArrow: '<button type="button" class="slick-prev"><img src="icon/press.png"></button>',
   nextArrow: '<button type="button" class="slick-next"><img src="icon/press.png"></button>',
 });
 
@@ -35,15 +35,46 @@ $('.post__slider2').slick({
   slidesToScroll: 1,
   asNavFor: '.post__slider1',
   // dots: true,
-  centerMode: true,
+  centerMode: false,
   arrows: false,
   infinite: true,
   focusOnSelect: true,
 
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        centerMode: false,
+  
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        centerMode: false,
+        vertical: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        centerMode: false,
+        vertical: true
+      }
+    }
 
+  ]
+});
 
 });
-})
+
 
 //slider 
 $(document).ready(function(){
@@ -53,11 +84,30 @@ $(document).ready(function(){
     slidesToShow: 2,
     slidesToScroll: 2,
     arrows: true,
-    prevArrow: '<button type="button" class="slick-prev"><img src="icon/quetes_left.png"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="icon/quetes_right.png"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="icon/left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icon/left.png"></button>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+         
+    
+        }
+      },
+      
+   
+  
+    ]
   });
+  
+  });
+  
+ 
 
-})
+
 
 
 
